0d2f468935b1aa347a8cfff75ffa9d1befdcac01
paulo.assis <paulo.assis@medgrupo.com.br>
2023-03-18 10:40:26 -0300

    Aplicado configuração inicial do backend

---
+++ backend/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "windows"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    eqeqeq: "error",
+    "no-duplicate-imports": "error",
+    "no-extra-parens": "error",
+  },
+};
