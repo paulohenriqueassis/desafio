9167ea1f685192a178c4e22226d5cf4d10f0a214
paulo.assis <paulo.assis@medgrupo.com.br>
2023-03-19 14:14:22 -0300

    refactor: aplicado ajuste retorno das requisições

--- backend/src/controllers/cards.controller.js
+++ backend/src/controllers/cards.controller.js
@@ -3,14 +3,13 @@ import CardService from "../services/cards.service.js";
 async function createCard(req, res, next) {
   try {
     const card = req.body;
-    if (!card.texto || !card.data_criacao || !card.tags || !card.tags.length) {
+    if (!card.texto || !card.data_criacao || !card.tags) {
       throw new Error(
         "Os campos texto, data de criação e tags são obrigatórios para a criação de um card."
       );
     }
-
     await CardService.createCard(card);
-    res.json("Card criado com sucesso.");
+    res.status(201).send();
   } catch (error) {
     next(error);
   }
@@ -18,7 +17,8 @@ async function createCard(req, res, next) {
 
 async function getCards(req, res, next) {
   try {
-    res.send(await CardService.getCards());
+    const cards = await CardService.getCards();
+    res.status(200).json(cards);
   } catch (error) {
     next(error);
   }
@@ -26,7 +26,9 @@ async function getCards(req, res, next) {
 
 async function getCardById(req, res, next) {
   try {
-    res.send(await CardService.getCardById(req.params.id));
+    const { id } = req.params;
+    const card = await CardService.getCardById(id);
+    res.status(200).json(card);
   } catch (error) {
     next(error);
   }
@@ -34,7 +36,9 @@ async function getCardById(req, res, next) {
 
 async function getCardByTagName(req, res, next) {
   try {
-    res.send(await CardService.getCardByTagName(req.params.name));
+    const { name } = req.params;
+    const card = await CardService.getCardByTagName(name);
+    res.status(200).json(card);
   } catch (error) {
     next(error);
   }
@@ -42,20 +46,20 @@ async function getCardByTagName(req, res, next) {
 
 async function updateCard(req, res, next) {
   try {
-    const { texto, data_modificacao, tags } = req.body;
-    if (!texto || !data_modificacao || tags) {
+    const { texto, data_modificacao } = req.body;
+    if (!texto || !data_modificacao) {
       throw new Error(
         "Os campos texto e data de modificação são obrigatórios para a atualização de um card."
       );
     }
-
+    const { id } = req.params;
     const data = {
-      id: req.params.id,
+      id,
       texto,
       data_modificacao,
     };
     await CardService.updateCard(data);
-    res.json("Card atualizado com sucesso.");
+    res.status(200).send();
   } catch (error) {
     next(error);
   }
@@ -63,8 +67,9 @@ async function updateCard(req, res, next) {
 
 async function deleteCard(req, res, next) {
   try {
-    await CardService.deleteCard(req.params.id);
-    res.json("Card deletado com sucesso.");
+    const { id } = req.params;
+    await CardService.deleteCard(id);
+    res.status(200).send();
   } catch (error) {
     next(error);
   }
