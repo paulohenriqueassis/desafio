35180e8f720d45075ce87e4da4072c62d6a765f0
paulo.assis <paulo.assis@medgrupo.com.br>
2023-03-20 15:43:42 -0300

    refactor: ajustado script de upload de csv

--- backend/src/uploads/uploads.js
+++ backend/src/uploads/uploads.js
@@ -2,18 +2,18 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import csvtojson from "csvtojson";
-import CardSchema from "../schemas/cards.schema.js";
-import { connect } from "../database/mongodb.js";
 import handleFileCsv from "./handleFileCsv.js";
+import CardsService from "../services/cards.service.js";
 
 csvtojson()
   .fromFile("cards.csv")
   .then(async (fileCsv) => {
-    const file = handleFileCsv(fileCsv);  
-    const mongoose = await connect(process.env.MONGO_LOCAL);
-    const Insights = mongoose.model("Insights", CardSchema);
-    Insights.collection.insertMany(file, (err) => {
-      if (err) return console.error(err);
-      console.log("Múltiplos documentos inseridos no banco.");
-    });
+    const cards = handleFileCsv(fileCsv);
+    for (const card of cards) {
+      try {
+        await CardsService.createCard(card);
+      } catch (error) {
+        console.log("Erro ao tentar subir dado no banco.");
+      }
+    }
   });
