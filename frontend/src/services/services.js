fff645ae27cde8fa93c79269a1c7a769363d8bd4
paulo.assis <paulo.assis@medgrupo.com.br>
2023-03-22 09:27:12 -0300

    chore: adicionado o axios

---
+++ frontend/src/services/services.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const url = 'http://localhost:3000';
+
+const axiosInstance = axios.create({
+  baseURL: url,
+});
+
+export const api = {
+  get(endpoint) {
+    return axiosInstance.get(endpoint);
+  },
+  post(endpoint, body) {
+    return axiosInstance.post(endpoint, body);
+  },
+  put(endpoint, body) {
+    return axiosInstance.put(endpoint, body);
+  },
+  delete(endpoint) {
+    console.log(endpoint)
+    return axiosInstance.delete(endpoint);
+  },
+};
